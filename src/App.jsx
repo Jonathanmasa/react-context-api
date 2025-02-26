15c03d08459605b27db9c30554fd2489153e0c54
Jonathanmasa <jonathanmasa97@gmail.com>
2025-02-26 15:24:31 +0100

    feat[import/axios-get-posts/fold]

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
-
+// import degli elementi della libreria di gestione delle rotte
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import use state use effect
+import { useState, useEffect } from "react";
+// import axios
+import axios from "axios";
 
 function App() {
+
+    const [post, setPost] = useState([]);
+
+    // Fetching dei dati
+    function fetchPosts() {
+      axios.get("http://localhost:3000/posts")
+        .then((res) => 
+          setPost(res.data)
+        )
+    }
+
+    useEffect(fetchPosts, []);
 
   return (
     <>
